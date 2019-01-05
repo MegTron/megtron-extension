3c8f876ecb360a92d5cb533190fc0db2660bd51a
z2xu <zhongerxu@gmail.com>
2019-01-04 22:34:34 -0800

    fix trongrid links for account and tx

--- ui/lib/account-link.js
+++ ui/lib/account-link.js
@@ -2,10 +2,10 @@ module.exports = function (address, network, assetKey = '') {
   const net = parseInt(network)
   let link
   switch (net) {
-    case 1: // main net
+    case 11111: // main net
       link = assetKey ? `https://tronscan.org/#/token/${assetKey}/${address}` : `https://tronscan.org/#/address/${address}`
       break
-    case 5: // shasta test net
+    case 1: // shasta test net
       link = assetKey ? `https://explorer.shasta.trongrid.io/token/${assetKey}/${address}` : `https://explorer.shasta.trongrid.io/address/${address}`
       break
     default:
--- ui/lib/tx-link.js
+++ ui/lib/tx-link.js
@@ -2,10 +2,10 @@ module.exports = function (tx, network) {
   const net = parseInt(network)
   let link
   switch (net) {
-    case 1: // main net
+    case 11111: // main net
       link = `https://tronscan.org/#/transaction/${tx}`
       break
-    case 5: // shasta test net
+    case 1: // shasta test net
       link = `https://explorer.shasta.trongrid.io/#/transaction/${tx}`
       break
     default:
